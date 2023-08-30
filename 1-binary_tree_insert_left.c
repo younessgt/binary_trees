96eb333528b793cbd070ffd1331f8517c19c58c8
leila111111 <leila_merzak@outlook.fr>
2023-08-30 22:47:27 +0000

    insert

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node, *temp;
+	binary_tree_t *new_node;
 
 	if (parent == NULL)
 		return (NULL);
